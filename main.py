9c080ea1568f02609e011e7e8423c3476e5284a4
fkomljenovic <filip.komljenovic@huddle.tech>
2021-05-09 18:38:27 +0200

    add util methods to agent, fix action and obs space

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import ray
+from gym.spaces import Box, Discrete
 from multiagent_env.envs import MultiAgentBeerGame
 from ray import tune
-from ray.rllib.agents import ppo
-from ray.rllib.agents.ppo import PPOTrainer
 from ray.tune import register_env
 
 
@@ -11,24 +9,25 @@ def create_env(args):
 
 
 env = create_env(None)
-
 register_env("mabeer-game", create_env)
-policies = {str(agent.name): (None, env.observation_space, env.action_space, {}) for agent in env.agents}
+
+obs_space = Box(low=0, high=float('inf'), shape=(28,))
+action_space = Box(low=0, high=float('inf'), shape=(1,))
+policies = {str(agent.name): (None, obs_space, Discrete(1), {}) for agent in env.agents}
 
 analysis = tune.run("PPO",
                     stop={"training_iteration": 1},
-                    checkpoint_freq=10,
                     reuse_actors=False,
                     checkpoint_at_end=True,
-                    config={"num_sgd_iter": 1,
-                            "env": "mabeer-game",
+                    config={"env": MultiAgentBeerGame,
                             "env_config": {},
                             "num_workers": 0,
-                            "multiagent": {
-                                "policies": policies,
-                                "policy_mapping_fn": (lambda agent_id: agent_id)
-                            }
+                            # "multiagent": {
+                            #     "policies": policies,
+                            #     "policy_mapping_fn": (lambda agent_id: agent_id)
+                            # }
                             })
+
 checkpoints = analysis.get_trial_checkpoints_paths(
     trial=analysis.get_best_trial("episode_reward_mean", mode='max'),
     metric="episode_reward_mean")
--- multiple_policies.py
+++ multiple_policies.py
@@ -1,7 +1,15 @@
+import os
+import sys
+import numpy as np
+
 import ray
+from gym.spaces import Box
 from multiagent_env.envs import MultiAgentBeerGame
-from ray.rllib.agents.ppo import PPOTrainer
+from ray.rllib.agents.ppo import PPOTrainer, PPOTorchPolicy, PPOTFPolicy
 from ray.tune import register_env
+from ray.tune.logger import pretty_print
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
 
 def create_env(args):
@@ -9,16 +17,21 @@ def create_env(args):
 
 
 env = create_env(None)
-
 register_env("mabeer-game", create_env)
-policies = {str(agent.name): (None, env.observation_space, env.action_space, {}) for agent in env.agents}
+
+obs_space = Box(low=0, high=np.finfo(np.float32).max, shape=(28,), dtype=np.float32)
+action_space = Box(low=0, high=np.finfo(np.float32).max, shape=(1,), dtype=np.float32)
+policies = {str(agent.name): (None, obs_space, action_space, {}) for agent in env.agents}
 
 ray.init()
 trainer = PPOTrainer(env="mabeer-game", config={
     "env_config": {},
-    "num_workers": 0, "multiagent": {
+    "num_workers": 0,
+    "multiagent": {
         "policies": policies,
         "policy_mapping_fn": (lambda agent_id: agent_id),
     },
 })
-trainer.train()
+
+result = trainer.train()
+print(pretty_print(result))
