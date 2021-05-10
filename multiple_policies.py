0674845292308faa5522088ba9bd49560c244528
fkomljenovic <filip.komljenovic@huddle.tech>
2021-05-10 08:12:28 +0200

    use env config in constructors

--- multiple_policies.py
+++ multiple_policies.py
@@ -1,32 +1,40 @@
 import os
-import sys
-import numpy as np
 
+import numpy as np
 import ray
 from gym.spaces import Box
+from gym_env.envs.agent import Agent
 from multiagent_env.envs import MultiAgentBeerGame
-from ray.rllib.agents.ppo import PPOTrainer, PPOTorchPolicy, PPOTFPolicy
+from ray.rllib.agents.ppo import PPOTrainer
 from ray.tune import register_env
 from ray.tune.logger import pretty_print
 
 os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
 
+N_AGENTS = 4
+OBSERVATIONS_TO_TRACK = 5
 
-def create_env(args):
-    return MultiAgentBeerGame(None)
+
+def create_env(config):
+    return MultiAgentBeerGame(config)
 
 
-env = create_env(None)
+env_config = {
+    "n_agents": N_AGENTS,
+    "observations_to_track": OBSERVATIONS_TO_TRACK
+}
+env = create_env(env_config)
 register_env("mabeer-game", create_env)
 
-obs_space = Box(low=0, high=np.finfo(np.float32).max, shape=(28,), dtype=np.float32)
+obs_space = Box(low=0, high=np.finfo(np.float32).max, shape=(OBSERVATIONS_TO_TRACK * Agent.N_OBSERVATIONS,),
+                dtype=np.float32)
 action_space = Box(low=0, high=np.finfo(np.float32).max, shape=(1,), dtype=np.float32)
 policies = {str(agent.name): (None, obs_space, action_space, {}) for agent in env.agents}
 
 ray.init()
 trainer = PPOTrainer(env="mabeer-game", config={
-    "env_config": {},
     "num_workers": 0,
+    "env_config":env_config,
     "multiagent": {
         "policies": policies,
         "policy_mapping_fn": (lambda agent_id: agent_id),
